a89d0c714929a9f82bdc2ef6355432a22d2106c2
tutu <tutugamestutubooks@gmail.com>
2017-07-03 15:01:55 +0900

    リスト6-16

---
+++ 7_3.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let list = document.getElementById('list');
+    console.log(list.innerHTML);
+    console.log(list.textContent);
+},false);
